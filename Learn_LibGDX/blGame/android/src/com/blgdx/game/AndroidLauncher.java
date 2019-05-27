02eab27001fefe63758967d7cc57e2bd5fea70e7
littleflute <littleflute@163.com>
2019-05-27 15:36:27 -0700

    + blGame

---
+++ Learn_LibGDX/blGame/android/src/com/blgdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.blgdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.blgdx.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ Learn_LibGDX/blGame/desktop/src/com/blgdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blgdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blgdx.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
