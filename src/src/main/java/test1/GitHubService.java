be823d1cf82e3738b0701af63040d312b5123361
littleflute <littleflute@163.com>
2019-05-28 18:26:24 -0700

    *GitHubService.java

--- src/src/main/java/test1/GitHubService.java
+++ src/src/main/java/test1/GitHubService.java
@@ -5,9 +5,9 @@ import retrofit2.Call;
 import retrofit2.http.*;
 import com.google.gson.JsonElement;
 import okhttp3.ResponseBody;
-//*
+ 
 public interface GitHubService {
   @GET("users/{person}/repos")
   Call<ResponseBody> listRepos(@Path("person") String user);
 }
-//*/
+ 
