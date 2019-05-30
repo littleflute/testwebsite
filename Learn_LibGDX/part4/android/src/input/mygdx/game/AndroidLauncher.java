909f8b7df452dc0bc8b16b0a597325d36c07b2ff
littleflute <littleflute@163.com>
2019-05-30 09:55:59 -0700

    part4

---
+++ Learn_LibGDX/part4/android/src/input/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package input.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import input.mygdx.game.InputGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new InputGdxGame(), config);
+	}
+}
---
+++ Learn_LibGDX/part4/desktop/src/input/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package input.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import input.mygdx.game.InputGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InputGdxGame(), config);
+	}
+}
