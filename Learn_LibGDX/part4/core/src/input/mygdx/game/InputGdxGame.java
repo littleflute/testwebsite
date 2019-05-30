3fe59d0a99a05984486c87714f1e54f536bb9f27
littleflute <littleflute@163.com>
2019-05-30 11:57:31 -0700

    part4 done.

--- Learn_LibGDX/part4/core/src/input/mygdx/game/InputGdxGame.java
+++ Learn_LibGDX/part4/core/src/input/mygdx/game/InputGdxGame.java
@@ -1,17 +1,25 @@
 package input.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+
+import com.badlogic.gdx.Input.Buttons;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class InputGdxGame extends ApplicationAdapter {
+public class InputGdxGame implements ApplicationListener,InputProcessor{
 	SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+
+	private float posX, posY;
 
 	@Override
 	public void create () {
@@ -61,5 +69,73 @@ public class InputGdxGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		texture.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		float moveAmount = 1.0f;
+		if(Gdx.input.isKeyPressed(Keys.CONTROL_LEFT))
+			moveAmount = 10.0f;
+
+		if(keycode == Keys.LEFT)
+			posX-=moveAmount;
+		if(keycode == Keys.RIGHT)
+			posX+=moveAmount;
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if(button == Buttons.LEFT){
+			posX = screenX - sprite.getWidth()/2;
+			posY = Gdx.graphics.getHeight() - screenY - sprite.getHeight()/2;
+		}
+		if(button == Buttons.RIGHT){
+			posX = Gdx.graphics.getWidth()/2 - sprite.getWidth()/2;
+			posY = Gdx.graphics.getHeight()/2 - sprite.getHeight()/2;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
