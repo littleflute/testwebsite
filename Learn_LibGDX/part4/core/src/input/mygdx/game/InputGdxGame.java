193ed62e5a69d6e220c5a99f0e05976c3586f6a6
littleflute <littleflute@163.com>
2019-05-30 10:04:25 -0700

    1 done.

--- Learn_LibGDX/part4/core/src/input/mygdx/game/InputGdxGame.java
+++ Learn_LibGDX/part4/core/src/input/mygdx/game/InputGdxGame.java
@@ -2,32 +2,57 @@ package input.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class InputGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	private Texture texture;
+	private Sprite sprite;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		batch = new SpriteBatch();
+
+		//texture = new Texture(Gdx.files.internal("data/0001.png"));
+		texture = new Texture("badlogic.jpg");
+		sprite = new Sprite(texture);
+		sprite.setPosition(w/2 -sprite.getWidth()/2, h/2 - sprite.getHeight()/2);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(-1f);
+			else
+				sprite.translateX(-10.0f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(1f);
+			else
+				sprite.translateX(10.0f);
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
 	}
 }
